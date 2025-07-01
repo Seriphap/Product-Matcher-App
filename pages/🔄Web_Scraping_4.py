b0ed39b5d4cfcaf17d4dae5e9f2bbf8da11c264c
Seriphap Siangnok (Way) <siangnok@gmail.com>
2025-07-01 22:23:54 +0700

    Update 🔄Web_Scraping_4.py

--- pages/🔄Web_Scraping_4.py
+++ pages/🔄Web_Scraping_4.py
@@ -74,10 +74,8 @@ if st.sidebar.button("🚀 Start Scraping"):
 
             elif selected_category == "Blow Molding Machines":
                 product_xpath = '//*[@id="8919138061"]/div/div/div/div/div[2]'
-                #image_xpath = './/div/div[1]/div[1]/a/img/@src'
-                #name_xpath = './/div/div[1]/div[1]/div[1]/text()'
-                image_xpath = './/div/div/div/div/div[2]/div/div[1]/div[1]/a'
-                name_xpath = './/div/div/div/div/div[2]/div/div[1]/div[1]/div[1]'
+                image_xpath = './/div/div[1]/div[1]/a'
+                name_xpath = './/div/div[1]/div[1]/div[1]'
                
             else:
                 product_xpath = '//*[@id="8919138061"]/div/div/div/div/div[2]/div/div'
